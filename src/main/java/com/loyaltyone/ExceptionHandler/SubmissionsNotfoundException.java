10e10a3ef0f2c2adc2766ffb21fa989ab6a46e06
abhigyan1 <abhigyandzapper@gmail.com>
2019-12-19 15:34:25 -0500

    change in application and html

--- src/main/java/com/loyaltyone/ExceptionHandler/SubmissionsNotfoundException.java
+++
@@ -1,7 +0,0 @@
-package com.loyaltyone.ExceptionHandler;
-
-public class SubmissionsNotfoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-//	ErrorMessage exceptionResponse  = new ErrorMes
-	
-}
--- src/main/java/com/loyaltyone/ExceptionHandler/TemperatureException.java
+++
@@ -1,5 +0,0 @@
-package com.loyaltyone.ExceptionHandler;
-
-public class TemperatureException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-}
